dee7d3246e37525dbfed310175fac7f9adf091df
Acer5740 <tand1@yandex.ru>
2018-03-30 21:52:57 +0500

    Add onClickListener for button

---
+++ app/src/main/java/ru/andreydom/alcatel_pbx/ActivityKonspektBase.java
@@ -0,0 +1,13 @@
+package ru.andreydom.alcatel_pbx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityKonspektBase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konspekt_base);
+    }
+}
--- app/src/main/java/ru/andreydom/alcatel_pbx/MainActivity.java
+++ app/src/main/java/ru/andreydom/alcatel_pbx/MainActivity.java
@@ -1,15 +1,63 @@
 package ru.andreydom.alcatel_pbx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static ru.andreydom.alcatel_pbx.R.id.button_konspekt_base;
+import static ru.andreydom.alcatel_pbx.R.id.button_konspekt_network;
+import static ru.andreydom.alcatel_pbx.R.id.button_kurs_base;
+import static ru.andreydom.alcatel_pbx.R.id.button_kurs_network;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnKursBase;
+    Button btnKursNetwork;
+    Button btnKonspektBase;
+    Button btnKonspektNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnKursBase = (Button)findViewById(R.id.button_kurs_base);
+        btnKursNetwork = findViewById(R.id.button_kurs_network);
+        btnKonspektBase = findViewById(button_konspekt_base);
+        btnKonspektNetwork = findViewById(button_konspekt_network);
+        btnKursBase.setOnClickListener(this);
+        btnKursNetwork.setOnClickListener(this);
+        btnKonspektBase.setOnClickListener(this);
+        btnKonspektNetwork.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case button_kurs_base:
+                Intent intent = new Intent(this, ActivityKursBase.class);
+                startActivity(intent);
+                break;
+
+            case button_kurs_network:
+                intent = new Intent(this, ActivityKursNetwork.class);
+                startActivity(intent);
+                break;
+
+            case button_konspekt_base:
+                intent = new Intent(this, ActivityKonspektBase.class);
+                startActivity(intent);
+                break;
+
+            case button_konspekt_network:
+                intent = new Intent(this, ActivityKonspektNetwork.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+        }
     }
 }
